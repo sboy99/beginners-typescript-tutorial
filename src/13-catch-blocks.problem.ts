98c8e4ad6cf9b03e1df346b682fbe58e2c084206
Sagar Bera <contact.sagarber@gmail.com>
2023-08-15 10:28:43 +0530

    chore: solved e-13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -1,15 +1,15 @@
 import { expect, it } from "vitest";
 
 const tryCatchDemo = (state: "fail" | "succeed") => {
-  try {
-    if (state === "fail") {
-      throw new Error("Failure!");
+    try {
+        if (state === "fail") {
+            throw new Error("Failure!");
+        }
+    } catch (e) {
+        if (e instanceof Error) return e.message;
     }
-  } catch (e) {
-    return e.message;
-  }
 };
 
 it("Should return the message when it fails", () => {
-  expect(tryCatchDemo("fail")).toEqual("Failure!");
+    expect(tryCatchDemo("fail")).toEqual("Failure!");
 });
